112b68ecde9bdf6dbebe0489d079e3d4f1e268ce
likithagithub <99140002+likithagithub@users.noreply.github.com>
2024-04-25 22:28:30 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,47 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which HTML tag is used to declare internal CSS?",
+    answer: "<style>",
+    options: [
+      "&lt;script&gt;",
+      "&lt;style&gt;",
+      "&lt;link&gt;",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How can we select an element with a specific ID in CSS?",
+    answer: "#",
+    options: [
+      "#",
+      ".",
+      "->",
+      "^"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The correct sequence of HTML tags for starting a webpage is -",
+    answer: "HTML, Head, Title, Body",
+    options: [
+      "Head, Title, HTML, Body",
+      "HTML, Body, Title, Head",
+      "HTML, Head, Title, Body",
+      "HTML, Title, Head, Body"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following element is responsible for making the text bold in HTML?",
+    answer: "<b>",
+    options: [
+        "&lt;pre&gt;",
+        "&lt;a&gt;",
+        "&lt;b&gt;",
+        "&lt;br&gt;"
+    ]
+  },
+    
+];
